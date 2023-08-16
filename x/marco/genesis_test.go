4a8b1a6166a2667d44ffaff6a823bb07afca7537
vuong177 <nguyenvuong1122000@gmail.com>
2023-08-16 13:13:43 +0700

    init repo

---
+++ x/marco/genesis_test.go
@@ -0,0 +1,29 @@
+package marco_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	keepertest "github.com/vuong177/marco/testutil/keeper"
+	"github.com/vuong177/marco/testutil/nullify"
+	"github.com/vuong177/marco/x/marco"
+	"github.com/vuong177/marco/x/marco/types"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.MarcoKeeper(t)
+	marco.InitGenesis(ctx, *k, genesisState)
+	got := marco.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
---
+++ x/marco/keeper/query.go
@@ -0,0 +1,7 @@
+package keeper
+
+import (
+	"github.com/vuong177/marco/x/marco/types"
+)
+
+var _ types.QueryServer = Keeper{}
